a934e0041f5e8800fef62a31eb8d37c7f0b60c93
swapnil_galande <sunny.galande@gmail.com>
2016-09-22 11:52:41 +0530

    Added practice project

---
+++ MavenPractice/src/main/java/com/script/SimpleParameter.java
@@ -0,0 +1,18 @@
+package com.script;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class SimpleParameter {
+	@Test
+	public void launchBrowser()
+	{
+		System.out.println("Inside First Test");
+		WebDriver driver=new ChromeDriver();
+		System.setProperty("webdriver.chrome.driver", "/Users/swapnil_galande/Documents/Drivers/chromedriver");
+		driver.get("http://www.gmail.com");
+		driver.quit();
+	}
+
+}
---
+++ MavenPractice/src/main/java/com/script/Test.java
@@ -0,0 +1,5 @@
+package com.script;
+
+public class Test {
+
+}
