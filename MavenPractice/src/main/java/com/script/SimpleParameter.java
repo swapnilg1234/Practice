b761e1ff3fede8c06e45c4d4e4d30cf281e2bc73
swapnil <sunny.galande@gmail.com>
2016-10-08 16:03:18 +0530

    Modified

--- MavenPractice/src/main/java/com/script/SimpleParameter.java
+++ MavenPractice/src/main/java/com/script/SimpleParameter.java
@@ -2,7 +2,6 @@ package com.script;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.annotations.Test;
 
 public class SimpleParameter {
@@ -11,7 +10,7 @@ public class SimpleParameter {
 	public void launchBrowser()
 	{
 		System.out.println("Inside First Tesjjkjt:::");
-		System.setProperty("webdriver.chrome.driver", "/Users/swapnil_galande/Downloads/chromedriver");
+		System.setProperty("webdriver.chrome.driver", "D:\\softwares\\Drivers\\chromedriver_win32\\chromedriver.exe");
 		WebDriver driver=new ChromeDriver();
 		driver.get("http://www.gmail.com");
 		driver.quit();
